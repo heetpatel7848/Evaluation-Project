b159820ac837ef3fd2b4de4270fdbb9ed0efd0a6
heetpatel7848 <heet.sabhaya@bacancy.com>
2023-08-12 17:44:15 +0530

    fetched the data from the backend of both apis with frontend complete

--- PracticalTask/Program.cs
+++ PracticalTask/Program.cs
@@ -15,6 +15,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("corsapp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddAutoMapper(typeof(MapperProfile));
 
 builder.Services.AddDbContext<PracticalTaskContext>(options =>
@@ -40,6 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("corsapp");
 
 app.UseAuthorization();
 
